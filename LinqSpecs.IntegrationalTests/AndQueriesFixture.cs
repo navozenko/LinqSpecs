bb85fc2b4a8b646fa95958a5963586546af03111
jfromaniello <jfromaniello@localhost>
2010-06-03 08:42:07 -0300

    fixed OrElse specification

---
+++ LinqSpecs.IntegrationalTests/AndQueriesFixture.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Chinook.Domain;
+using LinqSpecs.IntegrationalTests.SampleQueries;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Enumerable = System.Linq.Enumerable;
+
+namespace LinqSpecs.IntegrationalTests
+{
+	[TestFixture]
+	public class AndQueriesFixture
+	{
+		private ISessionFactory sf;
+		[SetUp]
+		public void SetUp()
+		{
+			var configuration = new Configuration().Configure();
+			sf = configuration.BuildSessionFactory();
+		}
+
+		[Test]
+		public void can_query_with_and_spec()
+		{
+
+			Specification<Album> and = new ByAlbumTitle { AlbumTitle = "Big Ones" }
+			                           & new ByArtistName { ArtistName = "Aerosmith" };
+
+			using(var s = sf.OpenSession())
+			using(var tx = s.BeginTransaction())
+			{
+				
+				var result = Enumerable.ToList<Album>(s.Query<Album>().Where(and.IsSatisfiedBy()));
+				result.Count.Should().Be.EqualTo(1);
+			}
+
+		}
+	}
+}
---
+++ LinqSpecs.IntegrationalTests/OrQueriesFixture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Chinook.Domain;
+using LinqSpecs.IntegrationalTests.SampleQueries;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace LinqSpecs.IntegrationalTests
+{
+	[TestFixture]
+	public class OrQueriesFixture
+	{
+		private ISessionFactory sf;
+		[SetUp]
+		public void SetUp()
+		{
+			var configuration = new Configuration().Configure();
+			sf = configuration.BuildSessionFactory();
+		}
+
+		[Test]
+		public void can_query_with_or_spec()
+		{
+
+			Specification<Album> or = new ByAlbumTitle { AlbumTitle = "Big Ones" }
+					| new ByArtistName { ArtistName = "Aerosmith" };
+
+			using (var s = sf.OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+
+				var result = s.Query<Album>().Where(or.IsSatisfiedBy()).ToList();
+				result.Should().Not.Be.Empty();
+			}
+
+		}
+	}
+	
+}
