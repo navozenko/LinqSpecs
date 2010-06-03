3ba7b5db3ba632b28ddd874e703c9c437b362d3d
jfromaniello <jfromaniello@localhost>
2010-06-03 08:34:19 -0300

    fixed AndSpecification for NHibernate Linq Provider. Thanks to Fabio Maulo.

--- LinqSpecs/AndSpecification.cs
+++ LinqSpecs/AndSpecification.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Linq.Expressions;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AndSpecification.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The and specification.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace LinqSpecs
 {
+	using System;
+	using System.Linq.Expressions;
+
+	/// <summary>
+	/// The and specification.
+	/// </summary>
+	/// <typeparam name="T">
+	/// </typeparam>
 	public class AndSpecification<T> : Specification<T>
 	{
 		private readonly Specification<T> spec1;
+
 		private readonly Specification<T> spec2;
 
 		public AndSpecification(Specification<T> spec1, Specification<T> spec2)
@@ -14,36 +29,53 @@ namespace LinqSpecs
 			this.spec2 = spec2;
 		}
 
-		public override Expression<Func<T, bool>> IsSatisfiedBy()
-		{
-			ParameterExpression param = Expression.Parameter(typeof(T), "x");
-			return Expression.Lambda<Func<T, bool>>(
-				Expression.AndAlso(
-					Expression.Invoke(spec1.IsSatisfiedBy(), param),
-					Expression.Invoke(spec2.IsSatisfiedBy(), param)), param);
-		}
-
 		public bool Equals(AndSpecification<T> other)
 		{
-			if (ReferenceEquals(null, other)) return false;
-			if (ReferenceEquals(this, other)) return true;
-			return Equals(other.spec1, spec1) && Equals(other.spec2, spec2);
+			if (ReferenceEquals(null, other))
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, other))
+			{
+				return true;
+			}
+
+			return Equals(other.spec1, this.spec1) && Equals(other.spec2, this.spec2);
 		}
 
 		public override bool Equals(object obj)
 		{
-			if (ReferenceEquals(null, obj)) return false;
-			if (ReferenceEquals(this, obj)) return true;
-			if (obj.GetType() != typeof (AndSpecification<T>)) return false;
-			return Equals((AndSpecification<T>) obj);
+			if (ReferenceEquals(null, obj))
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, obj))
+			{
+				return true;
+			}
+
+			if (obj.GetType() != typeof(AndSpecification<T>))
+			{
+				return false;
+			}
+
+			return this.Equals((AndSpecification<T>)obj);
 		}
 
 		public override int GetHashCode()
 		{
 			unchecked
 			{
-				return ((spec1 != null ? spec1.GetHashCode() : 0)*397) ^ (spec2 != null ? spec2.GetHashCode() : 0);
+				return ((this.spec1 != null ? this.spec1.GetHashCode() : 0) * 397) ^
+					   (this.spec2 != null ? this.spec2.GetHashCode() : 0);
 			}
+		}
+
+		public override Expression<Func<T, bool>> IsSatisfiedBy()
+		{
+			return this.spec1.IsSatisfiedBy().AndAlso(this.spec2.IsSatisfiedBy());
 		}
 	}
 }
