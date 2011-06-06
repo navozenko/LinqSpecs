7d220c6d23e1c826242878ee1d07c8ef3baffe7f
Jose F. Romaniello <Jose F. Romaniello@localhost>
2011-06-05 23:47:40 -0300

    implemented implict conversion operators as suggested by kzu

---
+++ LinqSpecs.Tests/BooleanOperators/ImplicitConversions.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace LinqSpecs.Tests.BooleanOperators
+{
+    [TestFixture]
+    public class ImplicitConversions
+    {
+        [Test]
+        public void can_implicitly_convert_spec_to_expr()
+        {
+            Expression<Func<string, bool>> expr = new AdHocSpecification<string>(s => s.Length == 2);
+            expr.Compile().Invoke("ar").Should().Be.True();
+        }
+
+        [Test]
+        public void can_implicitly_convert_expr_to_spec()
+        {
+            Expression<Func<string, bool>> expr = s => s.Length == 2;
+            Specification<string> spec = expr;
+            spec.IsSatisfiedBy().Compile().Invoke("ar").Should().Be.True();
+        }
+
+        [Test]
+        public void sample_0()
+        {
+            Expression<Func<string, bool>> expr1 = s => s.Length == 2;
+            Expression<Func<string, bool>> expr2 = s => s.StartsWith("a");
+            Expression<Func<string, bool>> expr = (Specification<string>)expr1 & expr2;
+
+            expr.Compile()("ar").Should().Be.True();
+        }
+        [Test]
+        public void sample_1()
+        {
+            Expression<Func<string, bool>> expr = s => s.Length == 2;
+            Specification<string> spec = expr;
+            Action<Expression<Func<string, bool>>> funcWithExprParam = e => e.Should().Be.SameInstanceAs(expr); //do nothing
+            funcWithExprParam(spec);
+        }
+
+
+        [Test]
+        public void sample_2()
+        {
+            var spec1 = new AdHocSpecification<string>(s => s.Length == 2);
+            var spec2 = new AdHocSpecification<string>(s => s.StartsWith("a"));
+            Expression<Func<string, bool>> expr = spec1 & spec2;
+
+            expr.Compile()("ar").Should().Be.True();
+        }
+
+        [Test]
+        public void sample_3()
+        {
+            var spec1 = new AdHocSpecification<string>(s => s.Length == 2);
+            Expression<Func<string, bool>> expr = spec1 & ((Expression<Func<string, bool>>)(s => s.StartsWith("a")));
+            expr.Compile()("ar").Should().Be.True();
+        }
+    }
+}
--- LinqSpecs/Specification.cs
+++ LinqSpecs/Specification.cs
@@ -8,22 +8,22 @@ namespace LinqSpecs
 	{
 		public abstract Expression<Func<T, bool>> IsSatisfiedBy();
 
-		public static Specification<T> operator &(Specification<T> spec1, Specification<T> spec2)
+	    public static bool operator false(Specification<T> spec1)
+	    {
+	        return false; // no-op. & and && do exactly the same thing.
+	    }
+
+	    public static bool operator true(Specification<T> spec1)
+	    {
+	        return false; // no - op. & and && do exactly the same thing.
+	    }
+
+	    public static Specification<T> operator &(Specification<T> spec1, Specification<T> spec2)
 		{
 			return new AndSpecification<T>(spec1, spec2);
 		}
 
-		public static bool operator false(Specification<T> spec1)
-		{
-			return false; // no-op. & and && do exactly the same thing.
-		}
-
-		public static bool operator true(Specification<T> spec1)
-		{
-			return false; // no - op. & and && do exactly the same thing.
-		}
-
-		public static Specification<T> operator |(Specification<T> spec1, Specification<T> spec2)
+	    public static Specification<T> operator |(Specification<T> spec1, Specification<T> spec2)
 		{
 			return new OrSpecification<T>(spec1, spec2);
 		}
@@ -32,6 +32,16 @@ namespace LinqSpecs
 		{
 			return new NegateSpecification<T>(spec1);
 		}
+
+        public static implicit operator Expression<Func<T, bool>>(Specification<T> spec)
+        {
+            return spec.IsSatisfiedBy();
+        }
+
+        public static implicit operator Specification<T>(Expression<Func<T, bool>> expr)
+        {
+            return new AdHocSpecification<T>(expr);
+        }
 
 		protected virtual object[] Parameters { get { return new object[] {Guid.NewGuid()}; } }
 
